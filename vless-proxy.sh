5e192bd186c691bf20a1a4ec3a237a79770fda2c
Shmiagkh <sexwithshreks@gmail.com>
2025-08-21 17:01:32 +0300

    Create vless-proxy.sh

---
+++ vless-proxy.sh
@@ -0,0 +1,98 @@
+#!bin/bash
+
+apt install curl mc htop nano
+
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+
+systemctl stop xray.service
+
+uuid=$(./usr/local/bin/xray uuid)
+
+keys=$(./usr/local/bin/xray x25519)
+
+read private_key public_key <<< "$keys"
+
+pass=$(openssl rand -base64 16)
+
+shortid=$(openssl rand -hex 8)
+
+ip=$(curl -s ifconfig.me)
+
+cat <<EOL > /usr/local/etc/xray/config.json
+{
+  "log": {
+    "loglevel": "info"
+  },
+  "routing": {
+    "rules": [],
+    "domainStrategy": "AsIs"
+  },
+  "inbounds": [
+    {
+      "port": 42638,
+      "tag": "ss",
+      "protocol": "shadowsocks",
+      "settings": {
+        "method": "2022-blake3-aes-128-gcm",
+        "password": "$pass",
+        "network": "tcp,udp"
+      }
+    },
+    {
+      "port": 443,
+      "protocol": "vless",
+      "tag": "vless_tls",
+      "settings": {
+        "clients": [
+          {
+            "id": "$uuid",
+            "email": "user@server",
+            "flow": "xtls-rprx-vision"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "reality",
+    "realitySettings": {
+      "show": false,
+      "dest": "www.microsoft.com:443",
+      "xver": 0,
+      "serverNames": [
+        "www.microsoft.com"
+      ],
+      "privateKey": "$private_key",
+      "minClientVer": "",
+      "maxClientVer": "",
+      "maxTimeDiff": 0,
+      "shortIds": [
+        "$shortid"
+      ]
+    }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "tag": "direct"
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "block"
+    }
+  ]
+}
+EOL
+
+echo "vless://$uuid@$ip:443?security=reality&encryption=none&pbk=$public_key&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=www.microsoft.com&sid=$shortid#vless"
+
+echo "ss://$pass@$ip:42638#ss22"
