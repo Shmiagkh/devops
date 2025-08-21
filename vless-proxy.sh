b9efdc7e4ddf335ba430effd781159817a218c4c
Shmiagkh <sexwithshreks@gmail.com>
2025-08-21 17:18:03 +0300

    Update vless-proxy.sh

--- vless-proxy.sh
+++ vless-proxy.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 apt install curl mc htop nano
 
@@ -92,7 +92,11 @@ cat <<EOL > /usr/local/etc/xray/config.json
   ]
 }
 EOL
-
+echo " "
 echo "vless://$uuid@$ip:443?security=reality&encryption=none&pbk=$public_key&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=www.microsoft.com&sid=$shortid#vless"
-
+ehco " "
 echo "ss://$pass@$ip:42638#ss22"
+
+systemctl start xray.service
+
+systemctl enable xray.service
